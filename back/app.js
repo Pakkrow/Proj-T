4417a363fc4cdc622ad79857f47489228cdbc3b6
unknown <marvin1.bellouard@epitech.eu>
2024-07-05 11:22:54 +0200

    [FIX] Pushed every necessary file

---
+++ back/app.js
@@ -0,0 +1,11 @@
+// Import the express module
+const express=require('express');
+// Create an instance of the express application
+const app=express();
+// Specify a port number for the server
+const port=5000;
+console.log("Heya");
+// Start the server and listen to the port
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
